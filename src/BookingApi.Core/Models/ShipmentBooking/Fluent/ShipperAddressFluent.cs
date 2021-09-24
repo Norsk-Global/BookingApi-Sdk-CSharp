a673395054b5d9ae33bab4ec2b812824e953d670
Chris Hayes <chris.hayes@norsk-global.com>
2021-09-24 16:45:32 +0100

    Added IOSS regex test

--- src/BookingApi.Core/Models/ShipmentBooking/Fluent/ShipperAddressFluent.cs
+++ src/BookingApi.Core/Models/ShipmentBooking/Fluent/ShipperAddressFluent.cs
@@ -1,4 +1,5 @@
 using BookingApi.Abstractions.Models.ShipmentBooking;
+using BookingApi.Core.Models.Validation;
 
 namespace BookingApi.Core.Models.ShipmentBooking.Fluent
 {
@@ -8,6 +9,8 @@ namespace BookingApi.Core.Models.ShipmentBooking.Fluent
         {
         }
 
+        [Matches(@"^IM[0-9]{3}[a-zA-Z0-9]{7}",
+            "Should conform the IOSS standard of: 12 Characters long, starting with IM and followed by 3 digits from the associated EU member state.")]
         public ShipperAddressFluent Ioss(string ioss)
         {
             Model.Ioss = Validate(ioss);
